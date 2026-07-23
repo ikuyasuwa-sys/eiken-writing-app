a0d4e32fd60bdcfb33a498eea9017d01eeef6a13
ikuyasuwa-sys <ikuya.suwa@gmail.com>
2026-07-23 13:21:51 +0900

    Update Firebase configuration with new credentials

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "ここにFirebaseのapiKey",
-  authDomain: "ここにauthDomain",
-  projectId: "ここにprojectId",
-  storageBucket: "ここにstorageBucket",
-  messagingSenderId: "ここにmessagingSenderId",
-  appId: "ここにappId"
+  apiKey: "AIzaSyD9a9-ZHQexstJcNkVHORq3swqLf1HR3QQ",
+  authDomain: "eiken-writing-app.firebaseapp.com",
+  projectId: "eiken-writing-app",
+  storageBucket: "eiken-writing-app.firebasestorage.app",
+  messagingSenderId: "312002399629",
+  appId: "1:312002399629:web:d736939a422854494a01c4"
 };
 
 const app = initializeApp(firebaseConfig);
